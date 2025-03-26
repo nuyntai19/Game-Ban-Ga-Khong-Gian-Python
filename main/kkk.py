4bdd0e48f2e542eeaf205e73292cc43c87c6ff46
SkinnySnail <dunguyendangkhoa12a8@gmail.com>
2025-03-27 00:37:41 +0700

    key bind

--- main/kkk.py
+++ main/kkk.py
@@ -1,78 +1,123 @@
-import pygame
+import sys
+import pygame as pg
 import random
-import menu
-from menu import *
-# Khởi tạo pygame
-pygame.init()
 
 # Cấu hình cửa sổ game
 WIDTH, HEIGHT = 1280, 720
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Chicken Invaders")
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Chicken Invaders")
 
 # Load hình nền
-background_img = pygame.image.load("data/background3.jpg")
-background = pygame.transform.scale(background_img, (WIDTH, HEIGHT))
+background_img = pg.image.load("data/background3.jpg")
+background = pg.transform.scale(background_img, (WIDTH, HEIGHT))
 
 # Load hình tàu vũ trụ   
-ship_img = pygame.image.load("data/phiThuyen2.png")
-ship = pygame.transform.scale(ship_img, (50, 50))
+ship_img = pg.image.load("data/phiThuyen2.png")
+ship = pg.transform.scale(ship_img, (50, 50))
 
 # Load hình gà
-chicken_img = pygame.image.load("data/ga.png")
-chicken_img = pygame.transform.scale(chicken_img, (50, 50))  
+chicken_img = pg.image.load("data/ga.png")
+chicken_img = pg.transform.scale(chicken_img, (50, 50))  
 
 # Load hình đạn
-bullet_img = pygame.image.load("data/dan.png")
-bullet = pygame.transform.scale(bullet_img, (60, 60))
+bullet_img = pg.image.load("data/dan.png")
+bullet = pg.transform.scale(bullet_img, (60, 60))
 
 #Load hình cục máu rơi bổ sung cho tàu
-heart_img = pygame.image.load("data/mauBoSung.png")
-heart_img = pygame.transform.scale(heart_img, (50, 50))
+heart_img = pg.image.load("data/mauBoSung.png")
+heart_img = pg.transform.scale(heart_img, (50, 50))
 
 # Đạn của gà
-enemy_bullet_img = pygame.image.load("data/dan_ga.png")
-enemy_bullet = pygame.transform.scale(enemy_bullet_img, (40, 40))
+enemy_bullet_img = pg.image.load("data/dan_ga.png")
+enemy_bullet = pg.transform.scale(enemy_bullet_img, (40, 40))
 
 #Đạn của boss lv 1
-boss_bullet_img = pygame.image.load("data/danBossLV1.png")
-boss_bullet = pygame.transform.scale(boss_bullet_img, (40, 40))
+boss_bullet_img = pg.image.load("data/danBossLV1.png")
+boss_bullet = pg.transform.scale(boss_bullet_img, (40, 40))
 
 # vụ nổ khi gà bị bắn
-explosion_img = pygame.image.load("data/no.png")
-explosion_img = pygame.transform.scale(explosion_img, (50, 50))
+explosion_img = pg.image.load("data/no.png")
+explosion_img = pg.transform.scale(explosion_img, (50, 50))
 explosions = []
 
 # Load hình boss
-boss_img = pygame.image.load("data/boss1.png")
-boss_img = pygame.transform.scale(boss_img, (100, 100))
+boss_img = pg.image.load("data/boss1.png")
+boss_img = pg.transform.scale(boss_img, (100, 100))
 
 # Load hình boss cấp 2
-boss_img_lv2 = pygame.image.load("data/boss2.png")
-boss_img_lv2 = pygame.transform.scale(boss_img_lv2, (120, 120))
+boss_img_lv2 = pg.image.load("data/boss2.png")
+boss_img_lv2 = pg.transform.scale(boss_img_lv2, (120, 120))
 
 # Font chữ hiển thị
-font = pygame.font.Font(None, 36)
 
-#Biến input
-# This dict maps actions to the corresponding key scancodes.
-input_map = menu.input_map
 
 # Hàm vẽ nút chơi lại
 def draw_restart_button():
     restart_text = FONT.render("REPLAY", True, (255, 255, 255))
-    pygame.draw.rect(screen, (0, 0, 255), (WIDTH // 2 - 50, HEIGHT // 2 + 50, 100, 40)) 
+    pg.draw.rect(screen, (0, 0, 255), (WIDTH // 2 - 50, HEIGHT // 2 + 50, 100, 40)) 
     text_rect = restart_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 70))
     screen.blit(restart_text, text_rect) # Hiển thị chữ "REPLAY" ở giữa nút chơi lại 
-    return pygame.Rect(WIDTH // 2 - 50, HEIGHT // 2 + 50, 100, 40)
+    return pg.Rect(WIDTH // 2 - 50, HEIGHT // 2 + 50, 100, 40)
 
-pygame.init()
-FONT = pygame.font.Font(None, 40)
+pg.init()
+FONT = pg.font.Font(None, 40)
 
-BG_COLOR = pygame.Color('gray12')
-GREEN = pygame.Color('lightseagreen')
+BG_COLOR = pg.Color('gray12')
+GREEN = pg.Color('lightseagreen')
 
-def run_game(input_map1 = input_map):
+
+def create_key_list(input_map):
+    """A list of surfaces of the action names + assigned keys, rects and the actions."""
+    key_list = []
+    for y, (action, value) in enumerate(input_map.items()):
+        surf = FONT.render('{}: {}'.format(action, pg.key.name(value)), True, GREEN)
+        rect = surf.get_rect(topleft=(40, y*40+20))
+        key_list.append([surf, rect, action])
+    return key_list
+
+
+def assignment_menu(input_map):
+    """Allow the user to change the key assignments in this menu.
+
+    The user can click on an action-key pair to select it and has to press
+    a keyboard key to assign it to the action in the `input_map` dict.
+    """
+    selected_action = None
+    key_list = create_key_list(input_map)
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            elif event.type == pg.KEYDOWN:
+                if selected_action is not None:
+                    # Assign the pg key to the action in the input_map dict.
+                    input_map[selected_action] = event.key
+                    selected_action = None
+                    # Need to re-render the surfaces.
+                    key_list = create_key_list(input_map)
+                if event.key == pg.K_ESCAPE:  # Leave the menu.
+                    # Return the updated input_map dict to the main function.
+                    return input_map
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                selected_action = None
+                for surf, rect, action in key_list:
+                    # See if the user clicked on one of the rects.
+                    if rect.collidepoint(event.pos):
+                        selected_action = action
+
+        screen.fill(BG_COLOR)
+        # Blit the action-key table. Draw a rect around the
+        # selected action.
+        for surf, rect, action in key_list:
+            screen.blit(surf, rect)
+            if selected_action == action:
+                pg.draw.rect(screen, GREEN, rect, 2)
+
+        pg.display.flip()
+
+
+def run_game():
     global chicken
     # Reset các biến game
     ship_x, ship_y = WIDTH // 2, HEIGHT - 100
@@ -85,7 +130,7 @@ def run_game(input_map1 = input_map):
     heart_speed = 0.3
 
     heart_spawn_delay = random.randint(15000, 20000)  
-    last_heart_spawn_time = pygame.time.get_ticks() 
+    last_heart_spawn_time = pg.time.get_ticks() 
 
     # Đạn của tàu và gà 
     bullets = []
@@ -98,10 +143,10 @@ def run_game(input_map1 = input_map):
     last_shot_time = 0
 
     enemy_fire_delay = 1010  # tốc độ bắn của gà
-    last_enemy_shot_time = pygame.time.get_ticks()
+    last_enemy_shot_time = pg.time.get_ticks()
 
     boss_bullet_delay = 700 # Tốc độ bắn của boss
-    last_boss_bullet_time = pygame.time.get_ticks()
+    last_boss_bullet_time = pg.time.get_ticks()
 
     score = 0 # Điểm số
     boss = None
@@ -113,30 +158,35 @@ def run_game(input_map1 = input_map):
 
     boss_level = 1
     boss_respawn_time = None
+    
+    # This dict maps actions to the corresponding key scancodes.
+    input_map = {'move right': pg.K_d, 'move left': pg.K_a}
+    
     while running:
         screen.blit(background, (0, 0))
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:  # Enter the key assignment menu.
-                    input_map1 = assignment_menu(input_map1)
-            if game_over and event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:  # Enter the key assignment menu.
+                    input_map = assignment_menu(input_map)
+            if game_over and event.type == pg.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = event.pos
                 if restart_button.collidepoint(mouse_x, mouse_y):
                     game_over = False  # Đặt lại trạng thái game
                     run_game()  # Chạy lại game
-        
+
+
         if not game_over:
             # Điều khiển tàu vũ trụ
-            keys = pygame.key.get_pressed()
-            if keys[input_map1['move right']] and ship_x > 0:
+            keys = pg.key.get_pressed()
+            if keys[input_map['move right']] and ship_x > 0:
                 ship_x += ship_speed
-            if keys[input_map1['move left']] and ship_x < WIDTH - ship.get_width():
+            if keys[input_map['move left']] and ship_x < WIDTH - ship.get_width():
                 ship_x -= ship_speed
-            if keys[pygame.K_SPACE]:
-                current_time = pygame.time.get_ticks()
+            if keys[pg.K_SPACE]:
+                current_time = pg.time.get_ticks()
                 if current_time - last_shot_time > fire_delay:
                     bullets.append([ship_x + ship.get_width() // 2 - bullet.get_width() // 2-2, ship_y - 40])
                     last_shot_time = current_time
@@ -163,7 +213,7 @@ def run_game(input_map1 = input_map):
 
 
             # Gà bắn đạn
-            current_time = pygame.time.get_ticks()
+            current_time = pg.time.get_ticks()
             visible_chickens = []  # Khởi tạo danh sách rỗng để tránh lỗi UnboundLocalError
 
             if current_time - last_enemy_shot_time > enemy_fire_delay and chickens:
@@ -179,7 +229,7 @@ def run_game(input_map1 = input_map):
 
             # Tạo cục máu rơi bổ sung cho tàu
             # Kiểm tra nếu đã đủ thời gian để tạo cục máu mới
-            current_time = pygame.time.get_ticks()
+            current_time = pg.time.get_ticks()
             if current_time - last_heart_spawn_time > heart_spawn_delay:
                 hearts.append([random.randint(0, WIDTH - 64), -random.randint(50, 300)])
                 last_heart_spawn_time = current_time  # Reset bộ đếm thời gian
@@ -218,7 +268,7 @@ def run_game(input_map1 = input_map):
                         explosions.append([
                             chickens[i][0] + chicken_img.get_width() // 2 - explosion_img.get_width() // 2,
                             chickens[i][1] + chicken_img.get_height() // 2 - explosion_img.get_height() // 2,
-                            pygame.time.get_ticks()
+                            pg.time.get_ticks()
                         ])
 
                         chickens[i] = [random.randint(0, WIDTH - 64), -random.randint(50, 300)]
@@ -238,13 +288,13 @@ def run_game(input_map1 = input_map):
                                 boss_speed = 0  # Đặt lại tốc độ của boss
                                 boss_health = 0  # Đặt lại máu của boss
                                 boss_level = 2
-                                boss_respawn_time = pygame.time.get_ticks()  # Đặt thời gian hồi sinh cho boss lv2
+                                boss_respawn_time = pg.time.get_ticks()  # Đặt thời gian hồi sinh cho boss lv2
                                 print(f"⏳ Boss lv2 sẽ hồi sinh sau 3s. boss_respawn_time = {boss_respawn_time}")
 
             # Xuất hiện boss lv2 khi tiêu diệt thêm 20 con gà (score đạt 40)
             # Xuất hiện boss lv2 sau khi boss lv1 bị tiêu diệt 3 giây
             if boss is None and boss_respawn_time is not None:
-                elapsed_time = pygame.time.get_ticks() - boss_respawn_time
+                elapsed_time = pg.time.get_ticks() - boss_respawn_time
                 print(f"⏳ Đã chờ {elapsed_time} ms để hồi sinh boss lv2")
                 
                 if elapsed_time > 3000:
@@ -267,7 +317,7 @@ def run_game(input_map1 = input_map):
             # Hiển thị vụ nổ
             for explosion in explosions[:]:
                 screen.blit(explosion_img, (explosion[0], explosion[1]))
-                if pygame.time.get_ticks() - explosion[2] > 500:  # Hiển thị trong 500ms
+                if pg.time.get_ticks() - explosion[2] > 500:  # Hiển thị trong 500ms
                     explosions.remove(explosion)        
 
             # Kiểm tra va chạm giữa đạn của gà và tàu
@@ -280,8 +330,8 @@ def run_game(input_map1 = input_map):
             if score >= 10 and boss is None and boss_level == 1:
                 boss = [WIDTH // 2 - 50, 50]
                 boss_speed = 0.5
-                boss_img = pygame.image.load("data/boss1.png")
-                boss_img = pygame.transform.scale(boss_img, (100, 100))  
+                boss_img = pg.image.load("data/boss1.png")
+                boss_img = pg.transform.scale(boss_img, (100, 100))  
 
 
 
@@ -324,46 +374,18 @@ def run_game(input_map1 = input_map):
             screen.blit(score_text, (WIDTH - 120, 10))
 
             # Hiển thị thanh máu
-            pygame.draw.rect(screen, (255, 0, 0), (10, 10, ship_health * 2, 20))
-            pygame.draw.rect(screen, (255, 255, 255), (10, 10, 200, 20), 2)
+            pg.draw.rect(screen, (255, 0, 0), (10, 10, ship_health * 2, 20))
+            pg.draw.rect(screen, (255, 255, 255), (10, 10, 200, 20), 2)
         else:
             # Hiển thị màn hình Game Over
             game_over_text = FONT.render("GAME OVER!", True, (255, 255, 255))
             screen.blit(game_over_text, (WIDTH // 2 - 80, HEIGHT // 2))
             restart_button = draw_restart_button()  # Vẽ nút chơi lại
 
-        pygame.display.update()
+        pg.display.update()
         
-# Main menu function
-def main_menu():
-    while True:
-        screen.blit(BG, (0, 0))
+        
 
-        # Draw title
-        screen.blit(BG, (0, 0))
-        screen.blit(text, textRect)
-
-        draw()
-        update()
-
-        start_button = buttons[0]
-        option_button = buttons[1]
-        quit_button = buttons[2]
-
-        pygame.display.update()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if start_button.rect.collidepoint(event.pos):
-                    print("Game Starting...")  # Placeholder for starting the game
-                    return run_game()
-                if quit_button.rect.collidepoint(event.pos):
-                    pygame.quit()
-                    sys.exit()
-                if option_button.rect.collidepoint(event.pos):
-                    options()  # Open options menu
 if __name__ == '__main__':
-    main_menu()
+    run_game()
+    pg.quit()
--- main/menu.py
+++ main/menu.py
@@ -1,6 +1,7 @@
 import pygame, sys
-from UI import Button
+from UI import Button,Button1
 
+input_map = {'move right': pygame.K_d, 'move left': pygame.K_a}
 # Initialize pygame
 pygame.init()
 
@@ -10,14 +11,14 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Chicken Invaders")
 
 # Load background image
-BG_img = pygame.image.load("game/data/menu_bg.jpg")
+BG_img = pygame.image.load("data/menu_bg.jpg")
 BG = pygame.transform.scale(BG_img, (WIDTH, HEIGHT))
 
 # Font setup
 Font = pygame.font.Font(None, 50)
 # Font chữ hiển thị
 
-font = pygame.font.SysFont("impact", 170)  # font name and size
+font = pygame.font.Font("fonts\RetroFont.ttf", 100)  # font name and size
 text = font.render("CHICKEN INVADERS", False, (100,255,100)) # surface for text
 textRect = text.get_rect()
 textRect.center = (WIDTH//2, HEIGHT//2 - 200)  # center of text is screen center
@@ -28,6 +29,11 @@ buttons = [
     Button(screen, (WIDTH // 2, HEIGHT // 2 + 150), "OPTIONS"),
     Button(screen, (WIDTH // 2, HEIGHT // 2 + 300), "QUIT"),
 ]
+buttons1 = [
+    Button1(screen, (WIDTH // 2, HEIGHT // 2 + 300), "Keybind")
+]
+
+
 
 # Function to draw buttons
 def draw():
@@ -38,39 +44,71 @@ def draw():
 def update():
     for button in buttons:
         button.update()
+def draw1():
+    for button1 in buttons1:
+        button1.draw1()
 
-# Main menu function
-def main_menu():
+# Update buttons when an event occurs
+def update1():
+    for button1 in buttons1:
+        button1.update1()
+        
+FONT = pygame.font.Font(None, 40)
+
+BG_COLOR = pygame.Color('gray12')
+GREEN = pygame.Color('lightseagreen')
+
+        
+def create_key_list(input_map = input_map):
+    """A list of surfaces of the action names + assigned keys, rects and the actions."""
+    key_list = []
+    for y, (action, value) in enumerate(input_map.items()):
+        surf = FONT.render('{}: {}'.format(action, pygame.key.name(value)), True, GREEN)
+        rect = surf.get_rect(topleft=(40, y*40+20))
+        key_list.append([surf, rect, action])
+    return key_list
+
+
+def assignment_menu(input_map = input_map):
+    """Allow the user to change the key assignments in this menu.
+
+    The user can click on an action-key pair to select it and has to press
+    a keyboard key to assign it to the action in the `input_map` dict.
+    """
+    selected_action = None
+    key_list = create_key_list(input_map)
     while True:
-        screen.blit(BG, (0, 0))
-
-        # Draw title
-        screen.blit(BG, (0, 0))
-        screen.blit(text, textRect)
-
-        draw()
-        update()
-
-        start_button = buttons[0]
-        option_button = buttons[1]
-        quit_button = buttons[2]
-
-        pygame.display.update()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if start_button.rect.collidepoint(event.pos):
-                    print("Game Starting...")  # Placeholder for starting the game
-                    return  
-                if quit_button.rect.collidepoint(event.pos):
-                    pygame.quit()
-                    sys.exit()
-                if option_button.rect.collidepoint(event.pos):
-                    options()  # Open options menu
+            elif event.type == pygame.KEYDOWN:
+                if selected_action is not None:
+                    # Assign the pygame key to the action in the input_map dict.
+                    input_map[selected_action] = event.key
+                    selected_action = None
+                    # Need to re-render the surfaces.
+                    key_list = create_key_list(input_map)
+                if event.key == pygame.K_ESCAPE:  # Leave the menu.
+                    # Return the updated input_map dict to the main function.
+                    return input_map
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                selected_action = None
+                for surf, rect, action in key_list:
+                    # See if the user clicked on one of the rects.
+                    if rect.collidepoint(event.pos):
+                        selected_action = action
 
+        screen.fill(BG_COLOR)
+        # Blit the action-key table. Draw a rect around the
+        # selected action.
+        for surf, rect, action in key_list:
+            screen.blit(surf, rect)
+            if selected_action == action:
+                pygame.draw.rect(screen, GREEN, rect, 2)
+
+        pygame.display.flip()        
+        
 # Options menu function
 def options():
     running = True
@@ -80,16 +118,21 @@ def options():
         # Render options text
         options_text = Font.render("OPTIONS MENU", True, (255, 255, 255))
         screen.blit(options_text, (WIDTH // 2 - 120, HEIGHT // 4))
+        
+        draw1()
+        update1()
+
+        keybind_button = buttons1[0]
 
         pygame.display.update()
-
+    
         for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if keybind_button.rect.collidepoint(event.pos):
+                    assignment_menu(input_map)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False  # Return to main menu
-
-# Run main menu
-main_menu()
